d1164300e4455131030020434432ef4fc2eaf5fb
peedrofariaa <pedrofariagaldino@gmail.com>
2026-04-01 18:39:48 -0300

    feat: url back

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -20,7 +20,7 @@ export default function LoginPage() {
     setError('')
 
     try {
-      const res = await fetch('http://localhost:3001/auth/login', {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/login`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(form),
