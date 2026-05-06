9534a625b958b5a73e6a92fefb2f37ae115ffa6a
peedrofariaa <pedrofariagaldino@gmail.com>
2026-05-06 13:18:48 -0300

    feat: api url

--- src/app/(dashboard)/products/new/page.tsx
+++ src/app/(dashboard)/products/new/page.tsx
@@ -78,7 +78,7 @@ export default function NewProductPage() {
     try {
       const token = localStorage.getItem('admin_token')
 
-      const res = await fetch('http://localhost:3001/products', {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
